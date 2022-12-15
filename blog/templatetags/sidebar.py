cf9cc0542ec6495b9563d3b8f85b5864f2543bba
Saeedshoh <52253289+Saeedshoh@users.noreply.github.com>
2022-12-15 20:27:53 +0500

    Облако тегов

--- blog/templatetags/sidebar.py
+++ blog/templatetags/sidebar.py
@@ -7,3 +7,8 @@ register = template.Library()
 def popular_posts(cnt = 3):
     posts = Post.objects.order_by('-views')[:cnt]
     return {"posts": posts}
+
+@register.inclusion_tag('blog/tags_tpl.html')
+def tags(cnt = 10):
+    tags = Tag.objects.all()
+    return {"tags": tags}
