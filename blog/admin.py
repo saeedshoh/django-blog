3a4a7de83a262b60229b4f7f982bb29c597f33a8
Saeedshoh <52253289+Saeedshoh@users.noreply.github.com>
2022-12-15 20:15:49 +0500

    Вывод популярных постов

--- blog/admin.py
+++ blog/admin.py
@@ -20,7 +20,7 @@ class PostAdmin(admin.ModelAdmin):
     prepopulated_fields = {"slug": ("title",)}
     save_as = True
     save_on_top = True
-    list_display = ('id', 'title', 'slug', 'category', 'created_at', 'get_photo')
+    list_display = ('id', 'title', 'slug', 'category', 'created_at', 'get_photo', 'views')
     list_display_links = ('id', 'title')
     search_fields = ('title',)
     list_filter = ('category','tags')
---
+++ blog/templatetags/sidebar.py
@@ -0,0 +1,9 @@
+from django import template
+from blog.models import Post, Tag
+
+register = template.Library()
+
+@register.inclusion_tag('blog/popular_posts_tpl.html')
+def popular_posts(cnt = 3):
+    posts = Post.objects.order_by('-views')[:cnt]
+    return {"posts": posts}
